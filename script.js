8696b075beb63f593156b3501663dcff6412f459
AndreaBarbuto99 <barbutoandrea99@gmail.com>
2025-12-19 22:07:07 +0100

    feat: added button to generate new emails without mainting the others

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
 
 
 const ulPage = document.getElementById("email-list");
+const myButton = document.getElementById("new-email-list");
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
 
 
@@ -23,3 +24,21 @@ for (let i = 0; i < 10; i++) {
     .finally (() =>{
     });
 }
+
+myButton.addEventListener("click", () => {
+    ulPage.innerHTML = "";
+    for (let i = 0; i < 10; i++) {
+    axios.get(endpoint)
+    .then(response => {
+       let emailGet = response.data.response;
+       
+       ulPage.innerHTML += `<li>${emailGet}</li>`
+       })
+    .catch(error =>{
+        console.error(error);
+    })
+    
+    .finally (() =>{
+    });
+}
+});
