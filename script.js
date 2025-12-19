739117dac4e4857ed1604fabbceb7478bbe0be2c
AndreaBarbuto99 <barbutoandrea99@gmail.com>
2025-12-19 21:57:58 +0100

    feat: Added print in page of random email get from API

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
 
 
 
-
+const ulPage = document.getElementById("email-list");
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
 
-axios.get(endpoint)
-.then(response => {
-    const result = response.data;
-})
 
-.catch(error =>{
-    console.error(error);
-})
-
-.then (() =>{
-
-});
+for (let i = 0; i < 10; i++) {
+    axios.get(endpoint)
+    .then(response => {
+       let emailGet = response.data.response;
+       ulPage.innerHTML += `<li>${emailGet}</li>`
+       })
+    
+    
+    .catch(error =>{
+        console.error(error);
+    })
+    
+    .finally (() =>{
+    });
+}
